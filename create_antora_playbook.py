56cece8de78ac6e6d0e8a141b473e4aa7b8a3899
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-05-05 09:10:41 +0300

    Test

--- create_antora_playbook.py
+++ create_antora_playbook.py
@@ -5,11 +5,12 @@ import os
 from pathlib import Path
 
 stub = yaml.safe_load(open("antora-playbook-stub.yml").read())
+runner_temp=os.getenv("RUNNER_TEMP")
 
-for tagfile in glob.glob(f"{os.getenv("RUNNER_TEMP")}/tags/*.json"):
+for tagfile in glob.glob(f"{runner_temp)/tags/*.json"):
     tags = json.loads(open(tagfile).read())
     name = Path(tagfile).stem
-    source = { "url": f"https://github.com/StrongestNumber9/{name}.git", "tags": tags, "branches": ["!*"] }
+    source = { "url": f"{runner_temp}/repos/{name}", "tags": tags, "branches": ["!*"] }
     stub["content"]["sources"].append(source)
 
 print(yaml.dump(stub))
