9b700cdf97416b24d0f7e03ff99619fe3d58d05e
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-05-05 09:12:15 +0300

    Fix

--- create_antora_playbook.py
+++ create_antora_playbook.py
@@ -7,7 +7,7 @@ from pathlib import Path
 stub = yaml.safe_load(open("antora-playbook-stub.yml").read())
 runner_temp=os.getenv("RUNNER_TEMP")
 
-for tagfile in glob.glob(f"{runner_temp)/tags/*.json"):
+for tagfile in glob.glob(f"{runner_temp}/tags/*.json"):
     tags = json.loads(open(tagfile).read())
     name = Path(tagfile).stem
     source = { "url": f"{runner_temp}/repos/{name}", "tags": tags, "branches": ["!*"] }
